073c3db659a2de2e88749e08582f1a142fbe8e05
ፚ Ꭷ Ꮢ Ꭷ ❥ | 𝘽𝙇𝘼𝘾𝙆_𝙎𝙃𝘼𝙍𝙆 🦈 <101139382+ZORO2045@users.noreply.github.com>
2025-01-07 18:43:26 +0200

    Create LottieGeneratorApplication.java

---
+++ src/main/java/com/example/lottiegenerator/LottieGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lottiegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottieGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottieGeneratorApplication.class, args);
+	}
+
+}
